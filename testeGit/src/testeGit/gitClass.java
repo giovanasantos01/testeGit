3a20731b7d570acf5a1266ae0fcdf6666c4ffbfa
giovanasantos01 <gva.santos01@gmail.com>
2014-08-13 14:35:20 -0300

    13\08

--- testeGit/src/testeGit/gitClass.java
+++ testeGit/src/testeGit/gitClass.java
@@ -4,7 +4,7 @@ public class gitClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("teteGIT");
+		System.out.println("teteGIT1308");
 
 	}
 
